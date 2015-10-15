b42a7b91edee4a25a987598f10757e3791d8cd02
Tommi Vikkula <tvikkula@gmail.com>
2015-10-15 22:55:24 +0300

    Added a fully functional main.py and added final document (not done yet).

---
+++ getdatadict.py
@@ -0,0 +1,27 @@
+from __future__ import division
+import pickle
+import pprint
+
+data_dict = pickle.load(open("data/final_project_dataset.pkl", "r") )
+# Remove outliers:
+data_dict.pop('TOTAL', None)
+data_dict.pop('THE TRAVEL AGENCY IN THE PARK', None)
+# Add that one feature:
+for key,value in data_dict.iteritems():
+    if value['from_this_person_to_poi'] == 'NaN':
+        value['from_this_person_to_poi'] = 0
+    if value['from_poi_to_this_person'] == 'NaN':
+        value['from_poi_to_this_person'] = 0
+    if value['to_messages'] == 'NaN':
+        value['to_messages'] = 0
+    if value['from_messages'] == 'NaN':
+        value['from_messages'] = 0
+    if (value['to_messages'] + value['from_messages'] == 0):
+        value['email_ratio_with_poi'] = 0
+    else:
+        value['email_ratio_with_poi'] = (int(value['from_this_person_to_poi'])+\
+                                     int(value['from_poi_to_this_person']))/\
+                                     (int(value['to_messages'])+\
+                                     int(value['from_messages']))
+
+pickle.dump(data_dict, open('data/own_data_dict.pkl', "w") )
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-
 import sys
 import pickle
 import pprint
@@ -9,7 +8,7 @@ sys.path.append("./learning/")
 from preprocess import *
 import ClassifyNB, ClassifySVM, RFClassifier, PCA
 from feature_format import targetFeatureSplit
-from tester import test_classifier
+from tester import test_classifier, dump_classifier_and_data
 
 ### Load the numpy array with the dataset
 data = np.load('data/enrondata_normalized.npy')
@@ -36,42 +35,19 @@ labels, features = targetFeatureSplit(fresh_data)
 features_train, features_test, labels_train, labels_test = \
     stratifiedShuffleSplit(features, labels)
 
-### Do some PCA
-pca = PCA.doPCA(features_train, n = 3)
-transformed_train = pca.transform(features_train)
-transformed_test = pca.transform(features_test)
-#features_train = transformed_train
-#features_test = transformed_test
-
-# Now we have only PCA features:
-#features_only_list = ['pca'+str(i) for i in range(len(features_train[0]))]
-print features_only_list
-
 # Get feature importance:
-importance, selector = getFeatureImportance(features_train, labels_train, features_only_list, k = 9)
-plt.figure()
-plotFeatureImportance(importance, features_only_list, plt)
-plt.show()
+importance, selector = getFeatureImportance(features_train, labels_train, features_only_list, k = 8)
 
-### Next try with selector
 selector_train = selector.transform(features_train)
 
 # Do some hyperparam validation:
 best_svc, svc_grid_scores = ClassifySVM.gridsearch(selector_train, labels_train)
-print("Best SVMC estimator found by grid search:")
-pprint.pprint(best_svc)
 
 
-# Do fits based on hyperparam validation
-nbfit = ClassifyNB.train(selector_train, labels_train)
 svmfit = ClassifySVM.train(selector_train, labels_train, best_svc)
 
-
-### Probably better to test with precision and recall:
-print 'Naive bayes:'
-test_classifier(nbfit, data)
 print 'SVM:'
 test_classifier(svmfit, data)
 
-
-dump_classifier_and_data(svmfit, my_dataset, features_list)
+data_dict = pickle.load(open("data/own_data_dict.pkl", "r") )
+dump_classifier_and_data(svmfit, data_dict, features_list)
